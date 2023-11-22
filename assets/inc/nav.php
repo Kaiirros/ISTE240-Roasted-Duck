2164a506e37ef41550ed7f2a279bbfeb7c87040d
zhi_jun <liangzhijun1216@gmail.com>
2023-11-21 21:31:08 -0500

    added nav bar

---
+++ assets/inc/nav.php
@@ -0,0 +1,41 @@
+
+<div class="nav">
+    <div class="seele">
+    <img
+        src="assets/images/seele.png"
+        alt="logo imge"
+        height="40px"
+        width="50px"
+    />
+    </div>
+
+    <div class="dropdown">
+    <a href="travel.html" onsubmit="return false"
+        ><button class="dropbtn">Home</button></a
+    >
+    </div>
+
+    <div class="dropdown">
+    <button class="dropbtn active" >Unix</button>
+    <div class="dropdown-content">
+        <a href="#" >Tutorials</a>
+        <a href="#" >Quizzes</a>
+        
+    </div>
+    </div>
+
+    <div class="dropdown">
+    <button class="dropbtn active" >Sponsors</button>
+    <div class="dropdown-content">
+        
+    </div>
+    </div>
+
+    <div class="dropdown">
+    <button class="dropbtn active" id="culture-link">Contact</button>
+    <div class="dropdown-content">
+        
+    </div>
+    </div>
+
+</div>
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+  $path='./';
+  $page='Roasted Duck';
+  include $path . 'assets/inc/header.php';
+?>
+
+<?php
+  $path='./';
+  $page='Roasted Duck';
+  include $path . 'assets/inc/nav.php';
+?>
+
+
+
+
+
+<?php
+  $path='./';
+  $page='Roasted Duck';
+  include $path . 'assets/inc/footer.php';
+?>
