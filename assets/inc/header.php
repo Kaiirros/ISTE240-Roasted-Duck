06662ca5c7b6f8fcac0e2704169986390a6e12ab
zhi_jun <liangzhijun1216@gmail.com>
2023-11-27 14:29:12 -0500

    added images

--- assets/inc/header.php
+++ assets/inc/header.php
@@ -6,6 +6,6 @@
   <title>Roasted Duck</title>
   <link rel="stylesheet" type="text/css" href="<?php echo $path;?>assets/css/style.css"/>
   <script type="text/javascript" src="<?php echo $path;?>assets/script/index.js"></script>
- 
+  <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'>
 </head>
 <body>
--- index.php
+++ index.php
@@ -11,7 +11,42 @@
 ?>
 
 
+<div>
+  <div class="grid">
+    <div class="hero">
+      <p>UNIX</p> 
+      <p class="p1">Roasted Duck</p>
+      <!-- <button><a id="learnMore" href="#learnMore"> Learn More</button> -->
+    </div>
 
+    <div>
+      <img src="assets/images/image.png" alt="picture" height="400px" width="800px">
+    </div>
+
+  </div>
+
+  <div class="grid1">
+    <div>
+      <p>Goal 1</p> 
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad praesentium commodi ipsum expedita sint aspernatur nostrum odit excepturi vero, similique animi neque placeat ratione dicta cum, at sed magni natus!</p>
+    </div>
+
+    <div>
+      <p>Goal 2</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur assumenda placeat nemo soluta nisi minima? Ea tempore alias odio id, culpa consequuntur odit veritatis aliquam quos ipsa omnis quaerat pariatur?</p>
+    </div>
+
+    <div>
+      <p>Goal 3</p> 
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id, ea ipsum minus ex illo labore ut voluptates accusantium repudiandae consectetur autem commodi dolore similique, molestiae, fugit consequatur aperiam! Vitae, laudantium!</p>
+    </div>
+
+    <div>
+      <p>Goal 4</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro iusto voluptatibus mollitia ipsam aliquam nulla rem asperiores error dolorem molestiae! Nulla temporibus adipisci incidunt culpa commodi voluptatibus, omnis atque nam?</p>
+    </div>
+  </div>
+</div>
 
 
 <?php
