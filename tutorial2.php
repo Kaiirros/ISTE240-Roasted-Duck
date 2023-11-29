e2b9460e2a9da6491c879263ba7a10834a4e6a6d
zhi_jun <liangzhijun1216@gmail.com>
2023-11-29 16:19:22 -0500

    tutorial 1

--- tutorial2.php
+++ tutorial2.php
@@ -10,7 +10,50 @@
   include $path . 'assets/inc/nav.php';
 ?>
 
+<div class="grid3">
+  <br>
+  <br>
+  <br>
+  <p class="one"> Tutorial 1</p>
+  <div class="grid5">
+    
+    <div>
+      <p class="container"></p>
+      <p> 1: Listing files and directories</p>
+    </div>
 
+    <div>
+      <p class="container"></p>
+      <p> 1: Listing files and directories</p>
+    </div>
+
+    <div>
+      <p class="container1"></p>
+      <p> 1: Listing files and directories</p>
+    </div>
+
+    <div>
+      <p class="container2"></p>
+      <p> 1: Listing files and directories</p>
+    </div>
+
+    <div>
+      <p class="container3"></p>
+      <p> 1: Listing files and directories</p>
+    </div>
+
+    <div>
+      <p class="container4"></p>
+      <p> 1: Listing files and directories</p>
+    </div>
+
+    <div>
+      <p class="container4"></p>
+      <p> 1: Listing files and directories</p>
+    </div>
+  </div>
+
+</div>
 
 <?php
   $path='./';
