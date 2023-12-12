de44681325a10e4435c2b5b586fb3c4021555e7b
SepticHornet <90657505+SepticHornet@users.noreply.github.com>
2023-12-12 13:53:07 -0500

    Added Quiz Page
    Quiz Functionality mostly there...
    Adding to quiz is modular; add new array per question, below others.  follow array format as written in document
    Edit values of array for new content 
    Quiz page must be styled further.

---
+++ Quizlanding.php
@@ -0,0 +1,55 @@
+<?php
+  $path='./';
+  $page='Roasted Duck';
+  include $path . 'assets/inc/header1.php';
+?>
+
+<?php
+  $path='./';
+  $page='Roasted Duck';
+  include $path . 'assets/inc/nav.php';
+?>
+
+<div>
+  <div class="grid1">
+    <div>
+      <p class="p">Quizzes </p>
+    </div>
+  </div>
+
+
+  <div class="grid2">
+
+      <div>
+        <p class="p1">Select Quiz Below </p>
+      </div>
+
+      <div class="tutorialsContainer">
+      <div class="grid3">
+        <div>
+          <p class="p2"><a href="quiz1.php" class="link">Quiz 1 </a></p>
+        </div>
+
+        <div>
+          <p class="p2"><a href="#" class="link">Quiz 2 </a></p>
+        </div>
+
+        <div>
+          <p class="p2"><a href="#" class="link">Quiz 3 </a></p>
+        </div>
+
+        <div>
+          <p class="p2"><a href="#" class="link">Quiz 4 </a></p>
+        </div>
+      </div>
+
+
+    </div>
+  </div>
+</div>
+
+<?php
+  $path='./';
+  $page='Roasted Duck';
+  include $path . 'assets/inc/footer.php';
+?>
