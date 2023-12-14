8c79e2e043db6cb9cca041501e5ab410561e9ec7
zhi_jun <liangzhijun1216@gmail.com>
2023-12-13 23:21:31 -0500

    contact complete

--- contact.php
+++ contact.php
@@ -30,7 +30,7 @@
             <img src="assets/images/n2_image.png" alt="n2" width="340px" height="300px" class="contact_image">
             <hr>
             <p class="about_title"> About me: </p>
-            <p class="about_text"> My name is Zhi jun liang, a college 2nd year student who is seeking a Co-op or internship in web-development. Availability in May 2024 - August 2024. I think I am qualified for this internship or co-op because I have a basic understanding of HTML, Javascript, CSS, and PHP and have worked on 2 projects. 1 project is a personal travel website while the other project is a teamwork project.</p>
+            <p class="about_text"> My name is Zhi jun liang, a college 2nd year student who is seeking a Co-op or internship in web-development. Availability in May 2024 - August 2024. I think I am qualified for this internship or co-op because I have a basic understanding of HTML, Javascript, CSS, and PHP and have worked on 2 projects. 1 project is a personal travel website while the other project is a teamwork project. I wanted to earn enough money.</p>
             <p class="contact_title"> Contact me at: </p>
 
             <a  class="contact_link" href="mailto:liangzhijun1216@gmail.com?subject=Subject%20Here&body=Your%20message%20here"><img src="assets/images/email.png" alt="Email" width="50px" height="40px"></a>
@@ -51,7 +51,7 @@
         <img src="assets/images/Daniel_img.jpg" alt="daniel" width="340px" height="300px" class="contact_image">
             <hr>
             <p class="about_title"> About me: </p>
-            <p class="about_text"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel omnis reprehenderit aspernatur laudantium nemo quod quidem odio sunt alias. Beatae, corporis aliquid. Corporis nam doloribus unde aperiam sapiente deleniti quaerat. Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, nihil sequi at blanditiis perspiciatis recusandae, praesentium accusantium quasi distinctio ab similique </p>
+            <p class="about_text"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel omnis reprehenderit aspernatur laudantium nemo quod quidem odio sunt alias. Beatae, corporis aliquid. Corporis nam doloribus unde aperiam sapiente deleniti quaerat. Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, nihil sequi at blanditiis perspiciatis recusandae, praesentium accusantium quasi distinctio ab similique. sofgnowenagfoa </p>
             <p class="contact_title"> Contact me at: </p>
             <a class="contact_link" href="mailto:liangzhijun1216@gmail.com?subject=Subject%20Here&body=Your%20message%20here"><img src="assets/images/email.png" alt="Email" width="50px" height="40px"></a>
         </div>
