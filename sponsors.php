bf2b6e3fea5528c90a311f28ebef98278c28a3a0
zhi_jun <liangzhijun1216@gmail.com>
2023-12-14 20:59:30 -0500

    finished sponsors

--- sponsors.php
+++ sponsors.php
@@ -17,33 +17,122 @@
 <br>
 <br>
 <br>
-<div class="profile-card">
-    <div class="profile-header">
-        <img src="assets/images/zark-profile.jpeg" alt="Zark Muckerberg" class="profile-photo">
-        <h1>Zark Muckerberg</h1>
-        <p>Age: 39</p>
-        <p>Occupation: Unemployed</p>
-        <p>Education: High School</p>
-        <p>Marital Status: Single</p>
-        <p>Location: Rochester, New York</p>
-    </div>
-    <div class="profile-biography">
-        <h2>Biography</h2>
-        <p>Zark is a friendly neighborhood man who often spends his days attempting to learn new skills. Recently, he learned of a platform called UNIX. He isn't fully sure what it is but he knows that it's very big in the technology scene.</p>
-    </div>
-    <div class="profile-goals">
-        <h2>Goals</h2>
-        <p>Zark is feeling very motivated to learn UNIX. Finally, he can focus on something that could be useful to him and possibly land him a job opportunity.</p>
-    </div>
-    <div class="profile-preferences">
-        <h2>Preferences</h2>
-        <p>Zark prefers information to be simple and straightforward.</p>
-    </div>
-    <div class="profile-traits">
-        <span class="trait friendly">Friendly</span>
-        <span class="trait curious">Curious</span>
-        <span class="trait reliable">Reliable</span>
-    </div>
+<div  class="profile-cards-container">
+  <div class="profile-card">
+      <div class="profile-header">
+          <img src="assets/images/zark-profile.jpeg" alt="Zark Muckerberg" class="profile-photo">
+          <h1>Zark Muckerberg</h1>
+          <p>Age: 39</p>
+          <p>Occupation: Unemployed</p>
+          <p>Education: High School</p>
+          <p>Marital Status: Single</p>
+          <p>Location: Rochester, New York</p>
+      </div>
+      <div class="profile-biography">
+          <h2>Biography</h2>
+          <p>Zark is a friendly neighborhood man who often spends his days attempting to learn new skills. Recently, he learned of a platform called UNIX. He isn't fully sure what it is but he knows that it's very big in the technology scene.</p>
+      </div>
+      <div class="profile-goals">
+          <h2>Goals</h2>
+          <p>Zark is feeling very motivated to learn UNIX. Finally, he can focus on something that could be useful to him and possibly land him a job opportunity.</p>
+      </div>
+      <div class="profile-preferences">
+          <h2>Preferences</h2>
+          <p>Zark prefers information to be simple and straightforward.</p>
+      </div>
+      <div class="profile-traits">
+          <span class="trait friendly">Friendly</span>
+          <span class="trait curious">Curious</span>
+          <span class="trait reliable">Reliable</span>
+      </div>
+  </div>
+
+  <div class="profile-card">
+      <div class="profile-header">
+          <img src="assets/images/a.png" alt="Zark Muckerberg" class="profile-photo">
+          <h1>Zark Muckerberg</h1>
+          <p>Age: 39</p>
+          <p>Occupation: Unemployed</p>
+          <p>Education: High School</p>
+          <p>Marital Status: Single</p>
+          <p>Location: Rochester, New York</p>
+      </div>
+      <div class="profile-biography">
+          <h2>Biography</h2>
+          <p>Zark is a friendly neighborhood man who often spends his days attempting to learn new skills. Recently, he learned of a platform called UNIX. He isn't fully sure what it is but he knows that it's very big in the technology scene.</p>
+      </div>
+      <div class="profile-goals">
+          <h2>Goals</h2>
+          <p>Zark is feeling very motivated to learn UNIX. Finally, he can focus on something that could be useful to him and possibly land him a job opportunity.</p>
+      </div>
+      <div class="profile-preferences">
+          <h2>Preferences</h2>
+          <p>Zark prefers information to be simple and straightforward.</p>
+      </div>
+      <div class="profile-traits">
+          <span class="trait friendly">Friendly</span>
+          <span class="trait curious">Curious</span>
+          <span class="trait reliable">Reliable</span>
+      </div>
+  </div>
+
+  <div class="profile-card">
+      <div class="profile-header">
+          <img src="assets/images/b.png" alt="Zark Muckerberg" class="profile-photo">
+          <h1>Zark Muckerberg</h1>
+          <p>Age: 39</p>
+          <p>Occupation: Unemployed</p>
+          <p>Education: High School</p>
+          <p>Marital Status: Single</p>
+          <p>Location: Rochester, New York</p>
+      </div>
+      <div class="profile-biography">
+          <h2>Biography</h2>
+          <p>Zark is a friendly neighborhood man who often spends his days attempting to learn new skills. Recently, he learned of a platform called UNIX. He isn't fully sure what it is but he knows that it's very big in the technology scene.</p>
+      </div>
+      <div class="profile-goals">
+          <h2>Goals</h2>
+          <p>Zark is feeling very motivated to learn UNIX. Finally, he can focus on something that could be useful to him and possibly land him a job opportunity.</p>
+      </div>
+      <div class="profile-preferences">
+          <h2>Preferences</h2>
+          <p>Zark prefers information to be simple and straightforward.</p>
+      </div>
+      <div class="profile-traits">
+          <span class="trait friendly">Friendly</span>
+          <span class="trait curious">Curious</span>
+          <span class="trait reliable">Reliable</span>
+      </div>
+  </div>
+
+  <div class="profile-card">
+      <div class="profile-header">
+          <img src="assets/images/c.png" alt="Zark Muckerberg" class="profile-photo">
+          <h1>Zark Muckerberg</h1>
+          <p>Age: 39</p>
+          <p>Occupation: Unemployed</p>
+          <p>Education: High School</p>
+          <p>Marital Status: Single</p>
+          <p>Location: Rochester, New York</p>
+      </div>
+      <div class="profile-biography">
+          <h2>Biography</h2>
+          <p>Zark is a friendly neighborhood man who often spends his days attempting to learn new skills. Recently, he learned of a platform called UNIX. He isn't fully sure what it is but he knows that it's very big in the technology scene.</p>
+      </div>
+      <div class="profile-goals">
+          <h2>Goals</h2>
+          <p>Zark is feeling very motivated to learn UNIX. Finally, he can focus on something that could be useful to him and possibly land him a job opportunity.</p>
+      </div>
+      <div class="profile-preferences">
+          <h2>Preferences</h2>
+          <p>Zark prefers information to be simple and straightforward.</p>
+      </div>
+      <div class="profile-traits">
+          <span class="trait friendly">Friendly</span>
+          <span class="trait curious">Curious</span>
+          <span class="trait reliable">Reliable</span>
+      </div>
+  </div>
 </div>
 
 
