3094498dbaa37a50d5daaa818d7fa5bcfa75f859
SepticHornet <90657505+SepticHornet@users.noreply.github.com>
2023-12-14 20:35:51 -0500

    Add files via upload
    Finished Quiz Functionality and updated css

---
+++ RoastedDuck/intro.php
@@ -0,0 +1,68 @@
+<?php
+  $path='./';
+  $page='Roasted Duck';
+  include $path . 'assets/inc/header.php';
+?>
+<body class="tutorial-body">
+<?php
+  $path='./';
+  $page='Roasted Duck';
+  include $path . 'assets/inc/nav.php';
+?>
+
+<div> 
+
+    <h1 class="intro-title"> Unix Introduction<h1>
+
+    <div class="info-flex-container">
+        <div class="info-section">
+            <p class="intro-title-info">What is UNIX? </p>
+            <p class="intro-info"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit laboriosam fuga ratione ad odio blanditiis. Accusantium, molestiae id veritatis repellendus, quaerat impedit facilis, sequi deleniti blanditiis commodi maiores! Nulla, reprehenderit?</p>
+        </div>
+
+        <div class="info-section">
+            <p class="intro-title-info">Why use UNIX? </p>
+            <p class="intro-info"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit laboriosam fuga ratione ad odio blanditiis. Accusantium, molestiae id veritatis repellendus, quaerat impedit facilis, sequi deleniti blanditiis commodi maiores! Nulla, reprehenderit?</p>
+        </div>
+
+        <div class="info-section">
+            <p class="intro-title-info">How to use UNIX? </p>
+            <p class="intro-info"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit laboriosam fuga ratione ad odio blanditiis. Accusantium, molestiae id veritatis repellendus, quaerat impedit facilis, sequi deleniti blanditiis commodi maiores! Nulla, reprehenderit?</p>
+        </div>
+    </div>
+
+    <hr>
+
+    <h1 class="intro-subtitle"> The UNIX Operating System</h1> 
+    <br>
+    <div class="info-flex-container"> 
+        <div>
+            <img src="assets/images/kernel.png" class="intro-photo"> 
+            <p><a href="https://www.pearsonhighered.com/assets/samplechapter/0/1/3/0/0130187062.pdf" target="_blank"> @ Credit the image </a> </p>
+            <p class="caption"> The Kernel</p>
+        </div>
+
+        <div>
+            <img src="assets/images/shell.png" width="460px" height="340px" class="intro-photo"> 
+            <p><a href="https://en.wikipedia.org/wiki/Version_6_Unix" target="_blank"> @ Credit the image </a> </p>
+            <p class="caption"> The Shell</p>
+        </div>
+
+        <div>
+            <img src="assets/images/directory_structure.png" width="460px" height="340px" class="intro-photo"> 
+            <p><a href="https://faculty.cs.niu.edu/~mcmahon/CS241/Notes/Unix_Reference/file_structure.html" target="_blank"> @ Credit the image </a> </p>
+            <p class="caption"> The Directory Structure</p>
+        </div>
+    </div>
+
+    <div class="tutorial_button">
+  
+        <a href="tutorial.php" class="unixbutton">Unix tutorials content</a>
+    </div>
+</div>
+
+<?php
+  $path='./';
+  $page='Roasted Duck';
+  include $path . 'assets/inc/footer.php';
+?>
